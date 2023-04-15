0726b32376a8aaf0d0dfe5d2de629439f3e96888
Roy Gero <roy.gero.ii@gmail.com>
2023-04-14 22:17:38 -0400

    Changing the edit so it works properly.

--- src/routes/workoutRoutes.js
+++ src/routes/workoutRoutes.js
@@ -62,9 +62,8 @@ router.delete('/workouts/:workoutID', async (req, res) => {
 
 router.put('/workouts/:workoutID', async (req, res) => {
     try { 
-        console.log("Hello");
         const currentUser = req.user._id;
-        const workoutID = req.body._id;
+        const workoutID = req.params.workoutID;
         const workout = await Workout.findOneAndUpdate({_id: workoutID, userID: currentUser}, req.body, {new: true})
         res.send(workout);
     } catch (err)
